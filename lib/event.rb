41a336cdfb8109e1a37618b7eec0dd64452df7c8
Arique Aguilar <arique1104@gmail.com>
2020-08-05 10:16:32 -0500

    Adds initialize method to event class

---
+++ lib/event.rb
@@ -0,0 +1,7 @@
+class Event
+ #attr_reader
+  def initialize(name)
+
+  end
+
+end
--- test/event_test.rb
+++ test/event_test.rb
@@ -7,9 +7,11 @@ require "./lib/event"
 
 class EventTest < Minitest::Test
 
-  #def test_it_exists
-    #assert_instance_of <Class>,
-  #end
+  def test_it_exists
+    event = Event.new("South Pearl Street Farmers Market")
+    
+    assert_instance_of Event, event
+  end
 
   #def test_it_has_attributes
   #end
