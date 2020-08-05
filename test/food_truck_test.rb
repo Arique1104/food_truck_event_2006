5eb9922b6a3b525e905a9e4b3dbc062dd9258048
Arique Aguilar <arique1104@gmail.com>
2020-08-05 10:04:07 -0500

    Adds required files to food truck test

---
+++ test/food_truck_test.rb
@@ -0,0 +1,16 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "mocha/minitest"
+require "./lib/item"
+require "./lib/food_truck"
+
+class FoodTruckTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
