2a35da2d8f7583d2441da8c0a7188c2693d52ade
Arique Aguilar <arique1104@gmail.com>
2020-08-05 10:05:11 -0500

    Adds initialize method to food trruck class

---
+++ lib/food_truck.rb
@@ -0,0 +1,7 @@
+class FoodTruck
+ #attr_reader
+  def initialize(name)
+
+  end
+
+end
--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -6,9 +6,11 @@ require "./lib/food_truck"
 
 class FoodTruckTest < Minitest::Test
 
-  #def test_it_exists
-    #assert_instance_of <Class>,
-  #end
+  def test_it_exists
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    
+    assert_instance_of FoodTruck, food_truck
+  end
 
   #def test_it_has_attributes
   #end
