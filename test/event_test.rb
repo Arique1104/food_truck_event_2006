c35ac8190cb1bccfc1cfa667d4ef86d286d0f63c
Arique Aguilar <arique1104@gmail.com>
2020-08-05 10:15:31 -0500

    Adds required files to Event class test

---
+++ test/event_test.rb
@@ -0,0 +1,17 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "mocha/minitest"
+require "./lib/item"
+require "./lib/food_truck"
+require "./lib/event"
+
+class EventTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
